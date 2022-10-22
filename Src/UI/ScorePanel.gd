022783913bfaaa2dc02d41d229c64cbe4ba491ac
JonathanWK <jonathanwk1383@gmail.com>
2022-10-23 00:21:49 +0700

    add player ui, connect finished screen (wip)

--- Src/UI/ScorePanel.gd
+++ Src/UI/ScorePanel.gd
@@ -4,11 +4,23 @@ onready var RankingLabel := $Polygon2D/Polygon2D2/HBoxContainer/RankingLabel
 onready var NameLabel := $Polygon2D/Polygon2D2/HBoxContainer/NameLabel
 onready var TimeLabel := $Polygon2D/Polygon2D2/HBoxContainer/TimeLabel
 
-var ranking := "1"
+var ranking := ""
 var racerName := ""
 var time := "--:--.---"
 
-func _ready():
+func _ready() -> void:
 	RankingLabel.text = ranking
 	NameLabel.text = racerName
 	TimeLabel.text = time
+
+func set_label_ranking(var value : int) -> void:
+	RankingLabel.text = String(value)
+
+
+func set_label_name(var value : String) -> void:
+	NameLabel.text = value
+
+func set_label_time(var value : String) -> void:
+	TimeLabel.text = value
+
+
