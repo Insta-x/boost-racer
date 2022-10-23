bfd73831bcab2ab1eab9f972fb3bb3c53af1e070
Insta-x <uttsadajason@outlook.com>
2022-10-23 21:49:51 +0700

    music button

--- Src/UI/Menu.gd
+++ Src/UI/Menu.gd
@@ -1,5 +1,13 @@
 extends Control
 
+
+onready var music_button := $MarginContainer/HBoxContainer/MusicButton
+
+
+func _ready() -> void:
+	music_button.pressed = not AudioServer.is_bus_mute(1)
+
+
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Src/Game.tscn")
 
@@ -10,3 +18,7 @@ func _on_ExitButton_pressed():
 
 func _on_TutorialButton_pressed():
 	get_tree().change_scene("res://Src/SampleWorld.tscn")
+
+
+func _on_MusicButton_toggled(button_pressed: bool) -> void:
+	AudioServer.set_bus_mute(1, not button_pressed)
